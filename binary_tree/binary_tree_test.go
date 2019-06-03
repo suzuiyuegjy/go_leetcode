d232bb57df84932bd49cec782759609a606c0ca2
raynor <jianyegu@126.com>
2019-06-03 16:02:07 +0800

    binary tree test

--- binary_tree/binary_tree_test.go
+++ binary_tree/binary_tree_test.go
@@ -7,6 +7,10 @@ import (
 	. "github.com/suzuiyuegjy/go_leetcode/binary_tree"
 )
 
+func node(val int) *TreeNode {
+	return &TreeNode{val, nil, nil}
+}
+
 var _ = Describe("BinaryTree", func() {
 	var s []string
 
@@ -20,6 +24,21 @@ var _ = Describe("BinaryTree", func() {
 				root := BuildTree(s)
 				s1 := ToSlice(root)
 				Expect(s).To(Equal(s1[:len(s)]))
+			})
+		})
+	})
+
+	Describe("LowestCommonAncestor", func() {
+		Context("when q not p's parnet", func() {
+			It("should equal to", func() {
+				root := BuildTree(s)
+				Expect((LowestCommonAncestor(root, node(5), node(1))).Val).To(Equal(3))
+			})
+		})
+		Context("when q is up's parnet", func() {
+			It("should equal to", func() {
+				root := BuildTree(s)
+				Expect((LowestCommonAncestor(root, node(5), node(4))).Val).To(Equal(5))
 			})
 		})
 	})
